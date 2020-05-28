8f932038b293c1e6707191266398d59a0e416d48
Darjan Divkovic <daarjan.tz@gmail.com>
2020-05-28 11:11:51 +0200

    Initial commit for TYP project!

---
+++ the-yummi-pizza/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import MenuItems from './components/MenuItems';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Yummi pizza!</h1>
+      <MenuItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ the-yummi-pizza/src/components/MenuItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MenuItems extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Menu Items</h2>
+            </div>
+        )
+    }
+}
